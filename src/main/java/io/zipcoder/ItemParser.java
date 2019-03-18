1ced76887efcb5b04777bce5e47e4340d53f26d3
Sean MacLaughlin <seanmaclaughlin@zipcoders-MacBook-Pro.local>
2019-03-18 16:29:07 -0400

    checkpoint 19/20

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,40 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ItemParser {
+
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        List<Item> nuList = new ArrayList<Item>();
+
+        String[] split1 = valueToParse.toLowerCase().split("##");
+        for (int i = 0; i < split1.length; i++) {
+            try {
+                nuList.add(parseSingleItem(split1[i]));
+            } catch (ItemParseException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+        return nuList;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        return null;
+        String[] splitter = singleItem.toLowerCase().split("[;#]");
+        try {
+            for (int i = 0; i < splitter.length; i++) {
+                splitter[i] = splitter[i].split("[:@^*%]")[1];
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new ItemParseException();
+        }
+
+        Item item = new Item(splitter[0], Double.valueOf(splitter[1]), splitter[2], splitter[3]);
+
+        return item;
     }
 }
